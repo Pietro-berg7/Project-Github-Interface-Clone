e30cfb6452bf4285e11dc5d76c3d86d006a26d4b
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-12-29 04:01:43 -0300

    Edita página Profile e cria componente ProfileData

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from "./styles";
+
+import { ProfileData } from "../../components/ProfileData";
 
 export const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={"Pietro-berg7"}
+            name={"Pietro Bergamaschi"}
+            avatarUrl={"https://avatars.githubusercontent.com/u/105403460?v=4"}
+            followers={7}
+            following={12}
+            company={"Trybe - Turma 25"}
+            location={"Santos Dumont - Minas Gerais"}
+            email={"pietrovb25@gmail.com"}
+            blog={"https://www.linkedin.com/in/pietroberg7/"}
+          />
+        </LeftSide>
+
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -3,3 +3,15 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+
+export const Main = styled.div`
+  
+`;
+
+export const LeftSide = styled.div`
+  
+`;
+
+export const RightSide = styled.div`
+  
+`;
