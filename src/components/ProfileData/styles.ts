a5a211979dc80e7d16f21bb4e173b93f8acab8e7
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-12-29 04:49:04 -0300

    Adiciona estilizações em Profile e ProfileData - Inicia componente RepoCard

--- src/components/ProfileData/styles.ts
+++ src/components/ProfileData/styles.ts
@@ -6,15 +6,65 @@ export const Container = styled.div`
 `;
 
 export const Flex = styled.div`
+  display: flex;
+  align-items: center;
 
+  > div {
+    margin-left: 24px;
+
+    > h1 {
+      font-size: 26px;
+      line-height: 1.25;
+      color: var(--gray-dark);
+      font-weight: 600;
+    }
+
+    > h2 {
+      font-size: 20px;
+      color: var(--username);
+      font-weight: 300;
+    }
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+
+    > div {
+      margin-left: 0;
+      margin-top: 16px;
+    }
+  }
 `;
 
 export const Avatar = styled.img`
+  width: 16%;
+  border-radius: 50%;
 
+  @media (min-width: 768px) {
+    width: 100%;
+  }
 `;
 
-export const Row = styled.div`
+export const Row = styled.ul`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 20px 0;
 
+  > li {
+    display: flex;
+    align-items: center;
+
+    > span {
+      font-size: 14px;
+      color: var(--gray);
+    }
+
+    > * {
+      margin-right: 5px;
+    }
+  }
 `;
 
 const iconCSS = css`
@@ -26,8 +76,23 @@ const iconCSS = css`
 
 export const PeopleIcon = styled(RiGroupLine)`${iconCSS}`;
 
-export const Column = styled.div`
+export const Column = styled.ul`
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
 
+  li + li {
+    margin-top: 0%;
+  }
+
+  span {
+    margin-left: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 `;
 
 export const CompanyIcon = styled(RiBuilding4Line)`${iconCSS}`;
