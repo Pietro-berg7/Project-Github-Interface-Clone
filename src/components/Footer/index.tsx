46a2365f8322c47de70a71a4dad63a34748c3624
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-01-02 20:14:10 +0044

    Cria componente footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container, Line, GithubLogo } from "./styles";
+
+export const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Line />
+      <GithubLogo />
+    </Container>
+  );
+};
